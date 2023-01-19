92315854bf1bf3abd1c6ade4dd14ea6166d2df4d
hi2102 <hi2102@naver.com>
2023-01-19 23:04:14 +0900

    movie app first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+    )
+      .then((res) => res.json())
+      .then((json) => setMovies(json.data.movies))
+      .then(setLoading(false));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div
+              key={movie.id}
+              style={{
+                display: 'flex',
+              }}
+            >
+              <div>
+                <img src={movie.medium_cover_image} />
+              </div>
+              <div
+                style={{
+                  paddingLeft: '1rem',
+                }}
+              >
+                <h2>Title - {movie.title}</h2>
+                <div>
+                  {movie.genres.map((g) => (
+                    <div key={g}>genres : {g}</div>
+                  ))}
+                  <span> rating : {movie.rating}</span>
+                </div>
+                <p>{movie.summary}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
